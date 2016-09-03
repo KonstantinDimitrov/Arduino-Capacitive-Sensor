1a313ae3429e63a075488d228754947fa2a57d7a
Konstantin Dimitrov <battlefield798@gmail.com>
2016-09-03 19:10:01 +0300

    Create Arduino-Capacitive-Sensor.ino

---
+++ Arduino-Capacitive-Sensor/Arduino-Capacitive-Sensor.ino
@@ -0,0 +1,24 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8); // 1M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+
+void setup()                    
+{
+   cs_4_8.set_CS_AutocaL_Millis(0xFFFFFFFF);// turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+   pinMode(7,OUTPUT);
+}
+
+void loop()                    
+{
+ long sensor1 =  cs_4_8.capacitiveSensor(50);
+
+    Serial.println(sensor1);  // print sensor output 
+   if(sensor1 >= 1000)
+   {
+    digitalWrite(7,HIGH);
+   }
+   else{
+    digitalWrite(7,LOW);
+   }  
+}
